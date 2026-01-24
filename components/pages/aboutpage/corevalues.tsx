c3c34be8fca97ff58a4a54b92d3687329d6c4551
adeka stanley <adekastanley2@gmail.com>
2026-01-24 17:17:57 +0100

    vercel fix

--- components/pages/aboutpage/corevalues.tsx
+++ components/pages/aboutpage/corevalues.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 export default function CoreValues() {
 	const coreValues = [
 		{

f87a9bd33b9c69651fdb77c997d4b67498883bdc
adeka stanley <adekastanley2@gmail.com>
2026-01-27 10:06:15 +0100

    fix responsive landing

--- components/pages/homepage/clients.tsx
+++ components/pages/homepage/clients.tsx
@@ -110,44 +110,46 @@ const logoCLoud = [
 ];
 export default function Clients() {
 	return (
-		<section className="bg-background pb-5">
-			<div className="group relative m-auto max-w-6xl px-6">
-				<div className="flex flex-col items-center md:flex-row">
-					<div className="md:max-w-44 md:border-r md:pr-6">
-						<p className="text-end text-sm">Our Clients</p>
-					</div>
-					<div className="relative py-6 md:w-[calc(100%-11rem)]">
-						<InfiniteSlider speedOnHover={20} speed={40} gap={112}>
-							{logoCLoud.map((item, index) => {
-								return (
-									<div key={index} className="flex">
-										<img
-											className="mx-auto h-12 w-fit dark:invert"
-											src={item.url}
-											alt={item.name}
-											height="24"
-											width="auto"
-										/>
-									</div>
-								);
-							})}
-						</InfiniteSlider>
+		<section className="w-full relative overflow-hidden">
+			<section className="bg-background pb-5 ">
+				<div className="group relative m-auto max-w-6xl px-6">
+					<div className="flex flex-col items-center md:flex-row">
+						<div className="md:max-w-44 md:border-r md:pr-6">
+							<p className="text-end text-sm">Our Clients</p>
+						</div>
+						<div className="relative py-6 md:w-[calc(100%-11rem)]">
+							<InfiniteSlider speedOnHover={20} speed={40} gap={112}>
+								{logoCLoud.map((item, index) => {
+									return (
+										<div key={index} className="flex">
+											<img
+												className="mx-auto h-12 w-fit dark:invert"
+												src={item.url}
+												alt={item.name}
+												height="24"
+												width="auto"
+											/>
+										</div>
+									);
+								})}
+							</InfiniteSlider>
 
-						<div className="bg-linear-to-r from-background absolute inset-y-0 left-0 w-20"></div>
-						<div className="bg-linear-to-l from-background absolute inset-y-0 right-0 w-20"></div>
-						<ProgressiveBlur
-							className="pointer-events-none absolute left-0 top-0 h-full w-20"
-							direction="left"
-							blurIntensity={1}
-						/>
-						<ProgressiveBlur
-							className="pointer-events-none absolute right-0 top-0 h-full w-20"
-							direction="right"
-							blurIntensity={1}
-						/>
+							<div className="bg-linear-to-r from-background absolute inset-y-0 left-0 w-20"></div>
+							<div className="bg-linear-to-l from-background absolute inset-y-0 right-0 w-20"></div>
+							<ProgressiveBlur
+								className="pointer-events-none absolute left-0 top-0 h-full w-20"
+								direction="left"
+								blurIntensity={1}
+							/>
+							<ProgressiveBlur
+								className="pointer-events-none absolute right-0 top-0 h-full w-20"
+								direction="right"
+								blurIntensity={1}
+							/>
+						</div>
 					</div>
 				</div>
-			</div>
+			</section>
 		</section>
 	);
 }

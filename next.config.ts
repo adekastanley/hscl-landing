afd3b80ca85d73b4d09b0f7dd1d8cad82c8ff96a
adeka stanley <adekastanley2@gmail.com>
2026-06-03 12:54:43 +0100

    update 2

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,26 @@ const nextConfig: NextConfig = {
 			bodySizeLimit: "15mb",
 		},
 	},
+	async rewrites() {
+		return [
+			{
+				source: "/partners/:path*",
+				destination: "/api/files/partners/:path*",
+			},
+			{
+				source: "/resources/:path*",
+				destination: "/api/files/resources/:path*",
+			},
+			{
+				source: "/siteimages/:path*",
+				destination: "/api/files/siteimages/:path*",
+			},
+			{
+				source: "/documents/:path*",
+				destination: "/api/files/documents/:path*",
+			},
+		];
+	},
 };
 
 export default nextConfig;
